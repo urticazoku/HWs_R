55f9ccf6e2e90918fe2bd0c9c058eee06d0852ac
urticazoku <urticazoku@users.noreply.github.com>
2016-05-13 15:15:26 +0500

    Update code2.R

--- week 2/code2.R
+++ week 2/code2.R
@@ -20,3 +20,82 @@ lb<-23
 ub<-25
 hx<-mean(x,m,sd)
 ans<-pnorm(ub,m,sd)-pnorm(lb,m,sd)
+
+install.packages("downloader")
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/mice_pheno.csv"
+filename <- basename(url)
+download(url, destfile=filename)
+dat <- read.csv(filename)
+dat <- na.omit( dat )
+
+x<-filter(dat, Diet=="chow" & Sex=="M") %>% select(Bodyweight) %>% unlist
+mean(x)
+
+#install.packages("rafalib")
+#library(rafalib)
+popsd(x)
+
+set.seed(1)
+X<-sample(x,25)
+mean(X)
+
+y<-filter(dat, Diet=="hf" & Sex=="M") %>% select(Bodyweight) %>% unlist
+mean(y)
+popsd(y)
+set.seed(1)
+Y<-sample(y,25)
+mean(Y)
+diff<-abs(mean(y)-mean(x))-abs(mean(X)-mean(Y))
+############
+x<-filter(dat, Diet=="chow" & Sex=="F") %>% select(Bodyweight) %>% unlist
+mean(x)
+
+#install.packages("rafalib")
+#library(rafalib)
+popsd(x)
+
+set.seed(1)
+X<-sample(x,25)
+mean(X)
+
+y<-filter(dat, Diet=="hf" & Sex=="F") %>% select(Bodyweight) %>% unlist
+mean(y)
+popsd(y)
+set.seed(1)
+Y<-sample(y,25)
+mean(Y)
+diff<-abs(mean(y)-mean(x))-abs(mean(X)-mean(Y))
+###############
+y<-filter(dat, Diet=="chow" & Sex=="M") %>% select(Bodyweight) %>% unlist
+z <- ( y - mean(y) ) / popsd(y)
+mean( abs(z) <=3 )
+
+qqnorm(z)
+abline(0,1)
+
+#############
+
+mypar(2,2)
+y <- filter(dat, Sex=="M" & Diet=="chow") %>% select(Bodyweight) %>% unlist
+z <- ( y - mean(y) ) / popsd(y)
+qqnorm(z);abline(0,1)
+y <- filter(dat, Sex=="F" & Diet=="chow") %>% select(Bodyweight) %>% unlist
+z <- ( y - mean(y) ) / popsd(y)
+qqnorm(z);abline(0,1)
+y <- filter(dat, Sex=="M" & Diet=="hf") %>% select(Bodyweight) %>% unlist
+z <- ( y - mean(y) ) / popsd(y)
+qqnorm(z);abline(0,1)
+y <- filter(dat, Sex=="F" & Diet=="hf") %>% select(Bodyweight) %>% unlist
+z <- ( y - mean(y) ) / popsd(y)
+qqnorm(z);abline(0,1)
+
+
+y <- filter(dat, Sex=="M" & Diet=="chow") %>% select(Bodyweight) %>% unlist
+avgs <- replicate(10000, mean( sample(y, 25)))
+mypar(1,2)
+hist(avgs)
+qqnorm(avgs)
+qqline(avgs)
+mean(avgs)
+popsd(avgs)
