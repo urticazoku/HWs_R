ef094f3b229088db2c51bc6c76c9a2134eede1b1
urticazoku <urticazoku@users.noreply.github.com>
2016-05-16 16:45:48 +0500

    Update code.R

--- week 3/code.R
+++ week 3/code.R
@@ -103,6 +103,105 @@ rejects <- replicate(10000,{
 })
 mean(rejects)
 
+#################
+#################
 
+set.seed(1)
+
+library(rafalib)
+dat<-read.csv("mice_pheno.csv")
+controlPopulation<-read.csv("femaleControlsPopulation.csv")
+controlPopulation<-unlist(controlPopulation)
+
+ttestgenerator<- function(n) {
+  cases<-sample(controlPopulation,n)
+  controls<-sample(controlPopulation,n)
+  tstat<-(mean(cases)-mean(controls))/sqrt(var(cases)/n+var(control)/n)
+  return(tstat)
+}
+ttests<-replicate(1000,ttestgenerator(10))
+hist(ttests)
+qqnorm(ttests)
+abline(0,1)
+
+ttests<-replicate(1000,ttestgenerator(3))
+hist(ttests)
+qqnorm(ttests)
+abline(0,1)
+
+ps<-(seq(0,999)+0.5)/1000
+qqplot(qt(ps,df=2*3-2),ttests,xlim=c(-6,6),ylim=c(-6,6))
+abline(0,1)
+
+qqnorm(controlPopulation)
+qqline(controlPopulation)
+
+controls<-rnorm(5000,mean=24,sd=3.5)
+
+ttestgenerator<-function(n,mean=24,sd=3.5){
+  cases<-rnorm(n,mean,sd)
+  controls<-rnorm(n,mean,sd)
+  tstat<-(mean(cases)-mean(controls))/sqrt(var(cases)/n+var(controls)/n)
+  return(tstat)
+}
+ttests<-replicate(1000,ttestgenerator(3))
+qqnorm(ttests)
+abline(0,1)
+
+########################
+set.seed(1)
+tgen<-function(n,mean=0,sd=1){
+  X<-rnorm(n,mean,sd)
+  tstat<-sqrt(5)*mean(X)/sd(X)
+  return(tstat)
+}
+set.seed(1)
+B<-1000
+ttests<-replicate(B,tgen(50))
+mean(ttests>2)
+
+set.seed(1)
+N <- 1000
+B <- 10000
+tstats <- replicate(B,{
+  X <-  sample(c(-1,1), N, replace=TRUE)
+  sqrt(N)*mean(X)/sd(X)
+})
+qqnorm(tstats)
+abline(0,1)
+
+######################
+
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/babies.txt"
+filename <- basename(url)
+download(url, destfile=filename)
+babies <- read.table("babies.txt", header=TRUE)
+bwt.nonsmoke <- filter(babies, smoke==0) %>% select(bwt) %>% unlist 
+bwt.smoke <- filter(babies, smoke==1) %>% select(bwt) %>% unlist
+
+N=10
+set.seed(1)
+nonsmokers <- sample(bwt.nonsmoke , N)
+smokers <- sample(bwt.smoke , N)
+obs <- median(smokers) - median(nonsmokers)
+
+set.seed(1)
+null <- replicate(1000, {
+  shuffle <- sample( dat )
+  smokersstar <- shuffle[1:N]
+  nonsmokersstar <- shuffle[(N+1):(2*N)]
+  median(smokersstar) - median(nonsmokersstar)
+})
+( sum( abs(null) >= abs(obs)) +1 ) / ( length(null)+1 ) 
+##we add the 1s to avoid p-values=0 but we also accept:
+( sum( abs(null) >= abs(obs)) ) / ( length(null) )
+
+##############
+##############
+
+d = read.csv("assoctest.csv")
+t<-table(d)
+chisq.test(t)
+fisher.test(t)
 
 
