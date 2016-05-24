7d7f6a834db2976a918edeb41f458cc7b1552974
urticazoku <urticazoku@users.noreply.github.com>
2016-05-24 19:43:52 +0500

    Update code.R

--- Part 2/week 4/code.R
+++ Part 2/week 4/code.R
@@ -59,3 +59,46 @@ f<-function(){
 set.seed(1)
 r<-replicate(1000,f())
 mean(r)
+
+###################
+###################
+
+sex <- factor(rep(c("female","male"),each=4))
+trt <- factor(c("A","A","B","B","C","C","D","D"))
+X <- model.matrix( ~ sex + trt)
+qr(X)$rank
+Y <- 1:8
+makeYstar <- function(a,b) Y - X[,2] * a - X[,5] * b
+fitTheRest <- function(a,b) {
+  Ystar <- makeYstar(a,b)
+  Xrest <- X[,-c(2,5)]
+  betarest <- solve(t(Xrest) %*% Xrest) %*% t(Xrest) %*% Ystar
+  residuals <- Ystar - Xrest %*% betarest
+  sum(residuals^2)
+}
+
+fitTheRest(1,2)
+
+expand.grid(1:3,1:3)
+betas = expand.grid(1:5,1:5)
+rss = apply(betas,1,function(x) fitTheRest(x[1],x[2]))
+rss
+
+library(rafalib)
+themin=min(rss)
+
+plot(betas[which(rss==themin),])
+
+#################
+
+fit <- lm(friction ~ type + leg, data=spider)
+betahat <- coef(fit)
+Y <- matrix(spider$friction, ncol=1)
+X <- model.matrix(~ type + leg, data=spider)
+QR <- qr(X)
+Q <- qr.Q( QR )
+R <- qr.R( QR )
+Q[1,1]
+R[1,1]
+betahat<-backsolve(R,crossprod(Q,Y))
+R%*%betahat
