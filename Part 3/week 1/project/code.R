1877c885c86aa107f2b41b201b44ed5b0de8320d
urticazoku <urticazoku@users.noreply.github.com>
2016-05-26 10:32:53 +0500

    Update code.R

--- Part 3/week 1/project/code.R
+++ Part 3/week 1/project/code.R
@@ -28,3 +28,46 @@ g <- factor(sampleInfo$group)
 ans<-apply(geneExpression,1,function(x,y) t.test( x[g==1], x[g==0])$p.value)
 min(ans)
 
+#######################
+set.seed(1)
+library(downloader)
+url = "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
+filename = "femaleControlsPopulation.csv"
+if (!file.exists(filename)) download(url,destfile=filename)
+population = read.csv(filename)
+pvals <- replicate(1000,{
+  control = sample(population[,1],12)
+  treatment = sample(population[,1],12)
+  t.test(treatment,control)$p.val
+})
+head(pvals)
+hist(pvals)
+
+mean(pvals<0.05)
+mean(pvals<0.01)
+
+cases = rnorm(10,30,2)
+controls = rnorm(10,30,2)
+t.test(cases,controls)
+
+set.seed(100)
+pvals<- replicate(20,{
+  cases = rnorm(10,30,2)
+  controls = rnorm(10,30,2)
+  t.test(cases,controls)$p.value
+})
+
+set.seed(100)
+pvals1<-replicate(1000,{
+  
+  pvals<-replicate(20,{
+    cases = rnorm(10,30,2)
+    controls = rnorm(10,30,2)
+    t.test(cases,controls)$p.value
+  })
+  
+  sum(pvals<=0.05)
+
+})
+mean(pvals1>0)
+
